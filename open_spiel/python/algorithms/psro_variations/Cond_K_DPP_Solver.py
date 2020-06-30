e6609b0fb4c6f6d760fa8f08c5de1067b9ab5bb0
oslumbers <40644337+oslumbers@users.noreply.github.com>
2020-06-30 11:59:00 +0100

    added K-DPP-Solver

---
+++ open_spiel/python/algorithms/psro_variations/Cond_K_DPP_Solver.py
@@ -0,0 +1,48 @@
+from numpy.random import RandomState
+import numpy as np
+from sklearn import preprocessing
+from sklearn.metrics.pairwise import linear_kernel
+
+from dppy.finite_dpps import FiniteDPP
+
+def indexed_identity(indices, kernel):
+
+    #Indices should be a list of the current population elements
+
+    size = kernel.shape[0]
+    I = np.eye(size)
+    for idx in indices:
+        I[idx, idx] = 0
+
+    return I
+
+def inner_term(kernel, current_popn):
+    I = indexed_identity(current_popn, kernel)
+    return np.linalg.inv(kernel + I)
+
+def outer_term(indices, kernel):
+
+    #Indices should be a list of the training population elements
+
+    return np.linalg.inv(kernel[np.ix_(indices, indices)])
+
+def conditional_k_dpp(meta_game, pop_size):
+
+    total_popn = list(range(meta_game.shape[0]))
+    current_popn = list(range(pop_size))
+    training_popn = [x for x in total_popn if x not in current_popn]
+
+    #normalise the meta_game
+    meta_game_normalised = preprocessing.normalize(meta_game, norm='l2')
+
+    #generate the L matrix
+    L = linear_kernel(meta_game_normalised)
+
+    #Generate the inner term
+    L_inner = inner_term(L, current_popn)
+
+    #Generate outer term
+    L_outer = outer_term(training_popn, L_inner)
+
+    #Return the final conditional K-DPP
+    return L_outer - np.eye(L_outer.shape[0])
